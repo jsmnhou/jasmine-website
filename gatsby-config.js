383517c7cecf49dd1fc72f1a8b6040e88fdfe667
jsmnhou <jsmnhou@umich.edu>
2023-05-10 22:13:04 -0700

    Removed excessive plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,67 +11,6 @@ module.exports = {
     // Meta
     // ===================================================================================
     "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [
-                    { "content:encoded": edge.node.html },
-                    { author: "hello@taniarascia.com" },
-                  ],
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 30,
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                  filter: { frontmatter: { template: { eq: "post" } } }
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { 
-                        slug 
-                      }
-                      frontmatter {
-                        title
-                        date
-                        template
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Tania Rascia | RSS Feed",
-          },
-        ],
-      },
-    },
     // ===================================================================================
     // Images and static
     // ===================================================================================
